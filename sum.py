0ea7113c081d7a6574d367492d2e21c8e65b3eab
Gulshan Kumar <132188220+GulshanZHI@users.noreply.github.com>
2024-04-16 01:03:21 +0530

    Update sum.py
    This is static/constant values

--- sum.py
+++ sum.py
@@ -1,3 +1,54 @@
-a=14
-b=45
-print(a+b)
+#Question 01
+
+x = 10
+y = "India"
+print("The output is = ", x)
+print('The output is = ', y)
+
+#Question 02
+
+a = 10
+A = "Python is case sensitive"
+print("The output is = ", a)
+print('The output is = ', A)
+
+#Question 03
+
+x = "India"
+y = "is"
+z = "Great"
+print(x, y, z)
+print(x + y + z)
+
+#Question 04
+
+x, y, z = "India", "is", "Great"   
+print(x, y, z)
+
+#Question 05
+
+x = y = z = "India is Great"   
+print(x, y, z)
+
+#Question 06
+
+x = 10
+y = 20
+z = x + y
+print("The addition result is = ", z)
+print('The addition result is = ', z)
+print(f"The addition result is =  {z}")
+
+#Question 07
+
+x = 10.52
+y = 20.32
+z = x + y
+print("The addition result is = ", z)
+
+#Question 08
+
+x = 10.52
+y = 20
+z = x + y
+print("The addition result is = ", z)
